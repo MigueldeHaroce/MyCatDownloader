9c92c51c43bed4127f6bd2b2d3cf0c173816ebc8
Miguel <migueldeharoce@gmail.com>
2022-11-29 13:26:27 +0100

    print("v2")

--- converter.py
+++ converter.py
@@ -6,7 +6,17 @@ def detect(myPath, myOutput):
                ".avi",
                ".mkv",
                ".webp",
-               ".mov"]
+               ".mov",
+               ".png",
+               ".jpg",
+               ".jpeg",
+               ".webp",
+               ".tiff",
+               ".mp3",
+               ".wac",
+               ".acc",
+               ".flac",
+               ".aiff"]
     for i in formats:
         if i in myPath:
             print("valid format")
@@ -17,22 +27,38 @@ def detect(myPath, myOutput):
     convert(nameFile, myPath, input_format, myOutput)
 
 def convert(name, myPath, myInput, output):
-    formatsDict = {".mp4":4,
-                   ".avi":4,
-                   ".mkv":4,
-                   ".webp":5,
-                   ".mov":4}
-    for i in formatsDict:
-        if formatsDict[i] == formatsDict[myInput]:
-            numbToEli = formatsDict[i]
-    print(numbToEli)
-    nameOnly = name[:numbToEli]
-    pathOnly = myPath[:-numbToEli]
+    
+    formatsDict = len(myInput)
+    print(formatsDict)
+    nameOnly = name[:formatsDict]
+    pathOnly = myPath[:-formatsDict]
     print(nameOnly)
+    
+    if myInput == ".mp4" or myInput == ".avi" or myInput == ".mkv" or myInput == ".webp" or myInput == ".mov":
+        if output == ".mp4" or output == ".avi" or output == ".mkv" or output == ".webp" or output == ".mov":
+            if myInput != output:
+                clip = moviepy.VideoFileClip(r"" + myPath)
+                result = moviepy.CompositeVideoClip([clip])
+                result.write_videofile(pathOnly + output, codec='mpeg4')
+            else:
+                # TODO POPUP
+                print("idk")
+    elif myInput == ".png" or myInput == ".jpg" or myInput == ".jpeg" or myInput == ".webp" or myInput == ".tiff":
+        if output == ".png" or output == ".jpg" or output == ".jpeg" or output == ".webp" or output == ".tiff":
+            if myInput != output:
+                pass
+                # TODO
+            # TODO
+        # TODO
+    elif myInput == ".mp3" or myInput == ".wac" or myInput == ".acc" or myInput == ".flac" or myInput == ".aiff":
+        if output == ".mp3" or output == ".wac" or output == ".acc" or output == ".flac" or output == ".aiff":
+            if myInput != output:
+                pass
+            # TODO
+        # TODO
 
-    clip = moviepy.VideoFileClip(r"" + myPath)
-    result = moviepy.CompositeVideoClip([clip])
-    result.write_videofile(pathOnly + output, codec='mpeg4')
+        
+
 
 if __name__ == "__main__":
     detect(r"C:\Users\migue\Desktop\myapp\test.avi", ".mkv")
